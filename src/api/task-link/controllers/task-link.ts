935b47a0d5b64d817aadeba2587a2c033ef69da2
sovaKrya <tyan_34ee@mail.ru>
2024-10-25 15:17:55 +0300

    corrected controller task-link

--- src/api/task-link/controllers/task-link.ts
+++ src/api/task-link/controllers/task-link.ts
@@ -2,6 +2,34 @@
  * task-link controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::task-link.task-link');
+export default factories.createCoreController(
+  "api::task-link.task-link",
+  ({ strapi }) => {
+    return {
+      async create(ctx) {
+        const body = JSON.parse(ctx.request.body);
+        const tasks = await strapi.documents("api::task.task").findMany({
+          filters: {
+            speciality: {
+              documentId: {
+                $eq: body.specialitys,
+              },
+            },
+          },
+        });
+
+        const uuid = crypto.randomUUID();
+
+        strapi.documents("api::task-link.task-link").create({
+          data: {
+            uuid: uuid,
+            validity: true,
+            task: tasks[Math.round(Math.random() * tasks.length)],
+          },
+        });
+      },
+    };
+  }
+);
